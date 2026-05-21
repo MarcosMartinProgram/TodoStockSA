4a10480bf6aa3dee1ddacf97e097d11d64dc2ad0
AgustinaBran <agusbrandemann@hotmail.com>
2026-05-20 21:01:16 -0300

    conexion a mongodb

---
+++ src/schemas/productSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  descripcion: { type: String },
+  precio: { type: Number, required: true, min: 0 },
+  stock: { type: Number, required: true, min: 0 },
+  proveedorId: { type: mongoose.Schema.Types.ObjectId, ref: 'Proveedor', required: true }
+});
+
+module.exports = mongoose.model('Producto', productSchema);
